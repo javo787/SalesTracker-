c54ba759238b7d9a6c9307297757395b0af10f54
javo787 <user.dr04@gmail.com>
2026-06-25 14:13:42 +0500

    Simplify metro.config.js by removing stream-browserify
    Removed stream-browserify from extraNodeModules.

--- metro.config.js
+++ metro.config.js
@@ -2,9 +2,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver.extraNodeModules = {
-  ...config.resolver.extraNodeModules,
-  stream: require.resolve('stream-browserify'),
-};
-
 module.exports = config;
